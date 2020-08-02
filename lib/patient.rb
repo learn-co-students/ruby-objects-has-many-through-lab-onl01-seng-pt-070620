466203097ac5dae33ce44908c7bd314186074973
Juliana Connors <juliana.g.connors@gmail.com>
2020-08-02 19:20:41 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Patient
+  attr_accessor :doctor, :appointment
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)  
+  end
+  
+  def appointments
+    Appointment.all.select{|app| app.patient == self}  
+  end
+  
+  def doctors
+    appointments.map{|appointment| appointment.doctor}  
+  end
+  
+end
