0fbb98a1f2a6a7212d30a7bc8326200c5e645eb3
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-08-05 17:54:05 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Patient
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+ def appointments
+     Appointment.all.select do |appointment|
+      appointment.patient == self
+     # binding.pry
+    end
+  end
+
+  def doctors
+    appointments.collect do |appointment|
+    appointment.doctor
+   # binding.pry
+  end
+end
+end
+
+
