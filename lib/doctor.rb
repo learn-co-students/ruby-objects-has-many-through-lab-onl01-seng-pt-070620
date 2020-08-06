4100f5d73cba4030d44d18107e578be231724cb3
Sean Ransonette <sransonette@gmail.com>
2020-08-06 04:32:23 +0000

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,24 @@
+class Doctor
+  
+  attr_accessor :name, :appointments, :patient
+  
+   @@all=[]
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments 
+    
+  end 
+
+  
+
+  
+end
